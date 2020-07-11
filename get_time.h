6f7e6aa85473b356db9360d6a304fe1734911cd2
luohui-code <1091992698@qq.com>
2020-07-11 22:16:22 +0800

    add Get time code

---
+++ get_time.h
@@ -0,0 +1,21 @@
+/********************************************************************************
+ *      Copyright:  (C) 2020 Luo Hui<1091992698@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  get_time.h
+ *    Description:  This head file 
+ *
+ *        Version:  1.0.0(2020年07月09日)
+ *         Author:  Luo Hui <1091992698@qq.com>
+ *      ChangeLog:  1, Release initial version on "2020年07月09日 13时20分14秒"
+ *                 
+ ********************************************************************************/
+
+#ifndef __GET_TIME_H_
+#define __GET_TIME_H_
+
+#include <stdio.h>
+#include <time.h>
+int get_time(char  *datime);
+
+#endif
